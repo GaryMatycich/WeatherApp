d2d429db52a44d14db5ab215f4acb791103315e2
Gary Matycich <garymatycich@gmail.com>
2020-09-07 23:47:31 -0600

    Added httpclient weather service to startup file.

--- WeatherApp.Web/Startup.cs
+++ WeatherApp.Web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WeatherApp.Infrastructure;
+using WeatherApp.Api.WeatherChannel;
 
 namespace WeatherApp.Web
 {
@@ -24,6 +26,7 @@ namespace WeatherApp.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient<IWeatherForcastService, WeatherChannelService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
